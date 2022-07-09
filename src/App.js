66ee1960c38073c917ea4c7e231bba9dd511a3e4
Иванов Тахир <watefalls@mail.ru>
2022-07-10 00:51:48 +0500

    progress--bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Redirect, Route, Switch} from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import NavBar from "./app/components/ui/navBar";
 import Breadcrumbs from "./app/components/common/breadcrumbs";
 import Main from "./app/components/layouts/main";
@@ -6,20 +6,28 @@ import NotFound from "./app/components/page/notFound";
 import UserPage from "./app/components/page/userPage";
 import Users from "./app/components/layouts/users";
 import Favorites from "./app/components/layouts/favorites";
+import ProgressBar from "./app/components/ui/progressBar";
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Breadcrumbs/>
+      <NavBar />
+      <Breadcrumbs />
+
       <Switch>
-        <Route path="/" exact component={Main}/>
-        <Route path="/user" component={Users}/>
-        <Route path="/user/:userId?" component={UserPage}/>
-        <Route path="/favorites" component={Favorites}/>
-        <Route path="/users/404" component={NotFound}/>
-        <Redirect to="404"/>
+        <Route path="/" exact component={Main} />
+        <Route path="/user" component={Users} />
+        <Route path="/user/:userId?" component={UserPage} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/users/404" component={NotFound} />
+        <Redirect to="404" />
       </Switch>
+      <ProgressBar percent={50} color="red" types="circle">
+        html
+      </ProgressBar>
+      <ProgressBar percent={50} color="green">
+        css
+      </ProgressBar>
     </div>
   );
 }
--- src/app/components/ui/progressBar.jsx
+++ src/app/components/ui/progressBar.jsx
@@ -1,1 +1,59 @@
-// Тахир
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProgressBar = ({ percent, color, types, children }) => {
+  const circleClass = () => (types === "circle" ? " circle" : "");
+
+  return (
+    <div className="progressBar">
+      <h2 className="progress__title">{children}</h2>
+      <div className={"progress__bar" + circleClass()}>
+        {types === "bar" ? (
+          <div
+            className="progress__inner"
+            style={{ backgroundColor: color, width: `${percent}%` }}
+          >
+            <p className="progres__percent">{percent}%</p>
+          </div>
+        ) : (
+          <div className={"progress__inner" + circleClass()}>
+            <p className={"progres__percent" + circleClass()}>{percent}%</p>
+          </div>
+        )}
+      </div>
+      {types === "circle" && (
+        <svg version="1.1" width="160px" height="160px">
+          <defs>
+            <linearGradient id="gradientColor">
+              <stop offset="0%" stopColor="#e91e63" />
+              <stop offset="100%" stopColor="#673ab7" />
+            </linearGradient>
+          </defs>
+          <circle
+            strokeDashoffset={472 - (472 / 100) * percent}
+            cx="80"
+            cy="80"
+            r="70"
+            strokeLinecap="round"
+          />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+ProgressBar.defaultProps = {
+  types: "bar",
+};
+
+ProgressBar.propTypes = {
+  percent: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  color: PropTypes.string,
+  types: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default ProgressBar;
