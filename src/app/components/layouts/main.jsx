528e88bea86f4229dcc11c62d5c2b8602ce8d75f
Notkabene <len1618@yandex.ru>
2022-07-10 00:23:44 +0300

    progressbar

--- src/app/components/layouts/main.jsx
+++ src/app/components/layouts/main.jsx
@@ -8,14 +8,24 @@ const Main = () => {
   return (
     <div className="main-wrapper">
       <Container>
-        <TextAnimation text={"Добро пожаловать!"}/>
+        <TextAnimation text={"Добро пожаловать!"} />
 
         <p className="mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dicta delectus quod voluptate quia corrupti accusamus, perferendis totam id nisi ipsum maiores sed repellendus unde, dolor maxime possimus veniam quisquam!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          dicta delectus quod voluptate quia corrupti accusamus, perferendis
+          totam id nisi ipsum maiores sed repellendus unde, dolor maxime
+          possimus veniam quisquam!
         </p>
 
         <UsersListPage />
       </Container>
+      <div
+        class="RadialProgress"
+        role="progressbar"
+        aria-valuenow="25"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >25%</div>
       <Wave />
     </div>
   );
--- src/app/components/page/usersListPage.jsx
+++ src/app/components/page/usersListPage.jsx
@@ -20,7 +20,6 @@ const UsersListPage = () => {
   }, []);
 
 
-
   if (users === undefined) {
     return "...loading";
   } else {
