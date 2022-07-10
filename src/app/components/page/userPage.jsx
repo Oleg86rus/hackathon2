fe7915fb93420eeac5a74d9df97fe73b3696854e
Sergey Fomin <gray7410@yandex.ru>
2022-07-10 11:28:11 +0500

    add user slider

--- src/app/components/page/userPage.jsx
+++ src/app/components/page/userPage.jsx
@@ -1,12 +1,13 @@
-import React, {useEffect, useState} from "react";
-import {Col, Container, Image, Row} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import PropTypes from "prop-types";
 import Line from "../common/line";
 import SocialNetworks from "../ui/socialNetworks";
 import LineBlack from "../common/lineBlack";
 import ButtonScrollTo from "../common/buttonScrollTo";
+import SliderBox from "../ui/slider";
 
-const UserPage = ({userId}) => {
+const UserPage = ({ userId }) => {
   const [user, setUser] = useState();
 
   useEffect(() => {
@@ -31,79 +32,83 @@ const UserPage = ({userId}) => {
               <Col>
                 <Row className="d-flex flex-column">
                   <Col>
-                    <Line className="mt-5"/>
+                    <Line className="mt-5" />
                     <h1 className="my-3">
                       {user.name} {user.surname}
                     </h1>
-                    <Line/>
+                    <Line />
                   </Col>
                   <Col className="mt-2 d-flex justify-content-between w-25">
-                    <SocialNetworks socialNetworks={user.socialNetworks}/>
+                    <SocialNetworks socialNetworks={user.socialNetworks} />
                   </Col>
                 </Row>
               </Col>
               <Col className="d-flex flex-column justify-content-end">
                 <h5>
                   Студент курса "Junior Frontend Developer Поток 3"
-                  <LineBlack/>
+                  <LineBlack />
                 </h5>
                 <p>
-                  Освоили тему React на уровне Junior, овладели
-                  основами профессии. Создали с помощью
-                  фреймворка React Single Application визуальную
-                  составляющую приложения: элементы и анимацию.
+                  Освоили тему React на уровне Junior, овладели основами
+                  профессии. Создали с помощью фреймворка React Single
+                  Application визуальную составляющую приложения: элементы и
+                  анимацию.
                 </p>
               </Col>
             </Row>
-            <ButtonScrollTo/>
+            <ButtonScrollTo />
           </Col>
           <Col xs={6} md={6} className="d-flex justify-content-center">
-            <Image src={user.img} className="w-75"/>
+            <Image src={user.img} className="w-75" />
           </Col>
         </Row>
         <Row className="mt-5 d-flex flex-column">
           <Col className="d-flex flex-column align-items-center">
-            <h2>
-              Вместе мы создаем решения
-            </h2>
+            <h2>Вместе мы создаем решения</h2>
             <h4 className="text-danger">
               Хакатон. Командная разработка по теме «React»
             </h4>
             <p>
-              Закрепляем знания модуля React, разработаем SPA
-              приложение на React благодаря командной работе.
+              Закрепляем знания модуля React, разработаем SPA приложение на
+              React благодаря командной работе.
             </p>
           </Col>
           <Col className="d-flex flex-column align-items-center">
-            <Line/>
+            <Line />
           </Col>
         </Row>
         <Row>
           <Col className="mt-3">
             <Row>
               <Col>
-                МЫ команда состоящая из программистов, дизайнеров, продакт-менеджеров и других
-                специалистов IT-сферы. Мы уже готовый коллектив, так и одиночные специалисты,
-                которые объедились непосредственно на мероприятии.
+                МЫ команда состоящая из программистов, дизайнеров,
+                продакт-менеджеров и других специалистов IT-сферы. Мы уже
+                готовый коллектив, так и одиночные специалисты, которые
+                объедились непосредственно на мероприятии.
               </Col>
               <Col>
-                Как только получили задание, сразу приступили к работе над кодингом для достижения поставленных целей.
-                Работа кипит почти без перерывов до тех пор, пока не истечет уделенное на соревнование время.
-                Царит довольно напряженная атмосфера, разработчики работают в течение длительных промежутков
-                времени без сна и останавливаются только для того, чтобы поесть и попить.
+                Как только получили задание, сразу приступили к работе над
+                кодингом для достижения поставленных целей. Работа кипит почти
+                без перерывов до тех пор, пока не истечет уделенное на
+                соревнование время. Царит довольно напряженная атмосфера,
+                разработчики работают в течение длительных промежутков времени
+                без сна и останавливаются только для того, чтобы поесть и
+                попить.
               </Col>
               <Col>
-                Я, как часть этого большого механизма сотворил:
-                !!!Задача организации, в особенности же дальнейшее развитие различных форм деятельности
-                представляет собой интересный эксперимент проверки дальнейших направлений развития.
-                Повседневная практика показывает, что сложившаяся структура организации способствует!!!
+                Я, как часть этого большого механизма сотворил: !!!Задача
+                организации, в особенности же дальнейшее развитие различных форм
+                деятельности представляет собой интересный эксперимент проверки
+                дальнейших направлений развития. Повседневная практика
+                показывает, что сложившаяся структура организации
+                способствует!!!
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className="mt-5">
           <Col>
-
+            <SliderBox userSlider={user.slider} />
           </Col>
         </Row>
       </Container>
@@ -112,7 +117,7 @@ const UserPage = ({userId}) => {
 };
 
 UserPage.propTypes = {
-  userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
 };
 
 export default UserPage;
