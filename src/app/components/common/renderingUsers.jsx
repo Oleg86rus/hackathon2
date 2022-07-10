74c68b173eae39d76179fc85e2f3efe488554266
Sergey Fomin <gray7410@yandex.ru>
2022-07-10 21:25:19 +0500

    1

--- src/app/components/common/renderingUsers.jsx
+++ src/app/components/common/renderingUsers.jsx
@@ -11,27 +11,32 @@ const RenderingUsers = ({ usersList }) => {
   const data = useSelector(getDataStatus());
   return (
     <>
-      {!data ? <Loading/> : (usersList.map((user) => (
-        <Card key={user.id} className="h-100 border-orange">
-          <div className="card-image">
-            <Card.Img variant="top" src={user.img} />
-          </div>
-          <Card.Body className="text-body">
-            <Badges userId={user.id} />
-            <Card.Title className="mt-2">
-              {user.name} {user.surname}
-            </Card.Title>
-            <Card.Text className="mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Officiis officia aperiam sequi tenetur iure eaque rem
-          repellendus impedit quasi quas possimus, enim, nam expedita
-          error, incidunt deleniti praesentium cumque corporis!
-            </Card.Text>
-            <OpenUserCard user={user}/>
-          </Card.Body>
-          <Bookmark userId={user.id}/>
-        </Card>
-      )))}
-    </>);
+      {!data ? (
+        <Loading />
+      ) : (
+        usersList.map((user) => (
+          <Card key={user.id} className="h-100 border-orange">
+            <div className="card-image">
+              <Card.Img variant="top" src={user.img} />
+            </div>
+            <Card.Body className="text-body">
+              <Badges userId={user.id} />
+              <Card.Title className="mt-2">
+                {user.name} {user.surname}
+              </Card.Title>
+              <Card.Text className="mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Officiis officia aperiam sequi tenetur iure eaque rem
+                repellendus impedit quasi quas possimus, enim, nam expedita
+                error, incidunt deleniti praesentium cumque corporis!
+              </Card.Text>
+              <OpenUserCard user={user} />
+            </Card.Body>
+            <Bookmark userId={user.id} users={usersList} />
+          </Card>
+        ))
+      )}
+    </>
+  );
 };
 export default RenderingUsers;
